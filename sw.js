0843a8797762e742140936f276a55163869091eb
Design-Effect <contact@designeffect3d.com>
2026-08-16 21:58:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'memory-v11';
+const CACHE = 'memory-v13';
 const ASSETS = ['./index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
